7156933a396cc9f2cc9c880ebe2f50b53d6e5524
Eugene Korir <korir16071@gmail.com>
2022-12-08 10:38:38 +0300

    added establish_connection function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod models;
+pub mod schema;
+// use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
+use diesel::{mysql::MysqlConnection, Connection};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn establish_connection() -> () {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    MysqlConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+        ()
 }
