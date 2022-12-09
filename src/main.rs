3400b88d5b654e82bc901ff53bf9feb2cd1f0bbf
Eugene Korir <korir16071@gmail.com>
2022-12-09 12:10:00 +0300

    modified main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,26 @@ use mysql_backend::*;
 fn main() {
     use self::schema::users::dsl::*;
 
-    let connection = &mut establish_connection();
+    let conn = &mut establish_connection();
+    
+    // add_user(conn, "Hariette Maina", "hariette@gmail.com");
     let results = users
-        .filter(admin.eq(false))
-        .limit(5)
-        .load::<User>(connection)
-        .expect("Error loading users");
-
-    println!("Displaying {} users", results.len());
-    for user in results {
-        println!("{:#?}", user);
+    .filter(admin.eq(false))
+    .limit(5)
+    .load::<User>(conn)
+    .expect("Error loading users");
+    
+    if results.len() > 0 {
+        if results.len() == 1 {
+            println!("Displaying {} user", results.len());
+        }else {
+            println!("Displaying {} users", results.len());
+        }
+        for user in results {
+            println!("{:#?}", user);
+        }
+    }else{
+        populate_database(conn);
     }
+
 }
