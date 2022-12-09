6d6f97c2ec1b488f83b37b3147b68a2de0015514
Eugene Korir <korir16071@gmail.com>
2022-12-09 12:12:10 +0300

    added add_user and populate_database functions

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,47 @@ pub mod models;
 pub mod schema;
 // use diesel::prelude::*;
 use dotenvy::dotenv;
-use std::env;
-use diesel::{mysql::MysqlConnection, Connection};
+use models::NewUser;
+use schema::users;
+use std::{env, collections::HashMap};
+use diesel::{mysql::MysqlConnection, Connection, RunQueryDsl};
+// use crate::models::User;
 
 
-pub fn establish_connection() -> () {
+pub fn establish_connection() -> MysqlConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     MysqlConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
-        ()
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))    
+}
+
+pub fn add_user(conn: &mut MysqlConnection,user_name: &str, email: &str)  {
+
+    let new_user = NewUser {
+        user_name,
+        email         
+    };
+    diesel::insert_into(users::table).values(&new_user).execute(conn).unwrap_or_else(|x| {
+        panic!("{x}");
+        
+    });
+}
+// #[memoize::memoize]
+pub fn populate_database(conn: &mut MysqlConnection) {
+    let users_list = HashMap::from([
+        ("Eugene","eugene@gmail.com"),
+        ("Sam","sam@gmail.com"),
+        ("Mike","mike@gmail.com")
+    ]);
+    for(user_name,email) in users_list {
+        diesel::insert_into(users::table).values(NewUser {
+        user_name,
+        email         
+    }).execute(conn).unwrap_or_else(|x| {
+        panic!("{x}");        
+    });
+    }
+    println!("Database populated!");
+
 }
