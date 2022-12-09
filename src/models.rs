403e3f6a9df636cea59dfdb7ba68ae2c1a9338e2
Eugene Korir <korir16071@gmail.com>
2022-12-09 12:11:01 +0300

    Added NewUseer struct

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,18 @@
+use diesel::prelude::*;
+use crate::schema::users;
+
+#[derive(Queryable,Debug)]
 pub struct User {
-    user_id: i32,
-    name: String,
-    email: String
+    pub user_id: i32,
+    pub name: String,
+    pub email: String,
+    pub admin: bool,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name= users)]
+pub struct NewUser<'a> {
+    // pub name: String,
+    pub user_name: &'a str,
+    pub email: &'a str,
 }
